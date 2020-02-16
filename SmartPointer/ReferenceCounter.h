0344d8e16427d79b2cfe60c26d723664037028bf
shoamco <shoamc98@gmail.com>
2020-02-16 04:49:45 -0800

    add function get and operators

--- SmartPointer/ReferenceCounter.h
+++ SmartPointer/ReferenceCounter.h
@@ -6,18 +6,18 @@
 #define CPP_UNDER_THE_HOOD_CONTROLBLOCK_H
 
 /**
- * class ControlBlock for Reference Counting :
+ * class ReferenceCounter for Reference Counting :
 It is a technique of storing the number of references, pointers or handles to a resource such as an object, block of memory, disk space or other resources.
 
 An object referenced by the contained raw pointer will not be destroyed until reference count is greater than zero
  until all copies of std::shared_ptr have been deleted.
  * */
-class ControlBlock {
+class ReferenceCounter {
 public:
-    ControlBlock():m_counter(0){};
+    ReferenceCounter(): m_counter(0){};
     //delete copy ctor and assignment operator because they do not want to allow
-    ControlBlock(const ControlBlock &)= delete;//delete copy ctor
-    ControlBlock& operator=(const ControlBlock&) = delete;   //delete copy ctor
+    ReferenceCounter(const ReferenceCounter &)= delete;//delete copy ctor
+    ReferenceCounter& operator=(const ReferenceCounter&) = delete;   //delete copy ctor
     size_t get_counter();
     // Overload post/pre increment
     void operator++();
@@ -26,36 +26,36 @@ public:
     void operator--();
     void operator--(int );
     // Overloading << operator
-    friend std::ostream& operator<<(std::ostream& os, const ControlBlock& counter);
+    friend std::ostream& operator<<(std::ostream& os, const ReferenceCounter& counter);
 private:
     size_t m_counter;
 };
 
-inline  size_t ControlBlock::get_counter(){
+inline  size_t ReferenceCounter::get_counter(){
     return m_counter;
 }
 // Overload post/pre increment
-inline void ControlBlock::operator++()
+inline void ReferenceCounter::operator++()
 {
     m_counter++;
 }
 
-inline void ControlBlock::operator++(int)
+inline void ReferenceCounter::operator++(int)
 {
     m_counter++;
 }
 
 // Overload post/pre decrement
-inline void ControlBlock::operator--()
+inline void ReferenceCounter::operator--()
 {
     m_counter--;
 }
-inline void ControlBlock::operator--(int)
+inline void ReferenceCounter::operator--(int)
 {
     m_counter--;
 }
 // Overloading << operator
-inline std::ostream& operator<<(std::ostream& os, const ControlBlock& counter)
+inline std::ostream& operator<<(std::ostream& os, const ReferenceCounter& counter)
 {
     os << "Counter Value : "
        << counter.m_counter << std::endl;
