09a727662648dbdba98e8418cea1951476a3190d
shoamco <shoamc98@gmail.com>
2020-02-16 04:54:15 -0800

    add test to check dtor in the end of scope

--- SmartPointer/Test/TestSmartPointer.cpp
+++ SmartPointer/Test/TestSmartPointer.cpp
@@ -22,7 +22,28 @@ void testCtorSmartPtr(){
 
 
 }
+void testSmartPtrInScope(){
+    std::cout<<"\n\n\n**************  testSmartPtrInScope *******\n";
+
+    SharedPointer<int> sharedPointer1(new int(5));
+     std::cout<<"sharedPointer1 "<<sharedPointer1.get()<<std::endl;
+    {
+        SharedPointer<int> sharedPointer2 = sharedPointer1;
+        std::cout << "sharedPointer2 " << sharedPointer2.get() << std::endl;
+        *sharedPointer2 = 10;
+        {
+            SharedPointer<int> sharedPointer3 = sharedPointer1;
+            std::cout << "sharedPointer3 " << sharedPointer3.get() << std::endl;
+            std::cout<<"end of scope \n";
+        }
+        std::cout<<"end of scope \n";
+
+    }
+    std::cout<<"after set sharedPointer1 "<<sharedPointer1.get()<<std::endl;
+
+}
 void testSmartPointer(){
      testControlBlock();
     testCtorSmartPtr();
+    testSmartPtrInScope();
 }
